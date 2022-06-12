af9a623a51cc29e18a3c788f30d2700b1eed202d
Elchin Gasimov <egasimov0@gmail.com>
2022-06-12 16:46:15 +0400

    Add more number level support, unit tests

--- helper/converter_test.go
+++ helper/converter_test.go
@@ -70,6 +70,54 @@ func Test_convertIntPart_WhereThreeDigitsNumber(t *testing.T) {
 	}
 }
 
+
+func Test_convertIntPart_WhereFourDigitsNumber(t *testing.T) {
+	fourDigitsCases := testCasesForFourDigitsNumbers()
+	for _, testCase := range fourDigitsCases {
+		actual := ConvertIntPart(testCase.given)
+
+		if !reflect.DeepEqual(actual, testCase.expected) {
+			//t.Error("For", testCase.description,
+			//	"\n Given: ", testCase.given,
+			//	"\n Expected: ", testCase.expected,
+			//	"\n Got: ", actual)
+			t.Errorf("For %s "+
+				"\n Given: %s, len: %d "+
+				"\n Expected: %s, len: %d"+
+				"\n Got: %s, len: %d",
+				testCase.description,
+				testCase.given, len(testCase.given),
+				testCase.expected, len(testCase.expected),
+				actual, len(actual))
+
+		}
+	}
+}
+
+func Test_convertIntPart_WhereFiveDigitsNumber(t *testing.T) {
+	fiveDigitsCases := testCasesForFiveDigitsNumbers()
+	for _, testCase := range fiveDigitsCases {
+		actual := ConvertIntPart(testCase.given)
+
+		if !reflect.DeepEqual(actual, testCase.expected) {
+			//t.Error("For", testCase.description,
+			//	"\n Given: ", testCase.given,
+			//	"\n Expected: ", testCase.expected,
+			//	"\n Got: ", actual)
+			t.Errorf("For %s "+
+				"\n Given: %s, len: %d "+
+				"\n Expected: %s, len: %d"+
+				"\n Got: %s, len: %d",
+				testCase.description,
+				testCase.given, len(testCase.given),
+				testCase.expected, len(testCase.expected),
+				actual, len(actual))
+
+		}
+	}
+}
+
+
 func testCasesForSingleDigitsNumbers() []testCase {
 	return []testCase{
 		testCase{
@@ -248,5 +296,133 @@ func testCasesForThreeDigitsNumbers() []testCase {
 			description: "Test Case: Single two digits number",
 			given:       "906",
 			expected:    "doqquz yüz altı",
+		}}
+}
+
+func testCasesForFourDigitsNumbers() []testCase {
+	return []testCase{
+		testCase{
+			description: "Test Case: four digits number",
+			given:       "1000",
+			expected:    "bir min",
+		},
+		testCase{
+			description: "Test Case: four digits number",
+			given:       "1111",
+			expected:    "bir min bir yüz on bir",
+		},
+		testCase{
+			description: "Test Case: four digits number",
+			given:       "1019",
+			expected:    "bir min on doqquz",
+		},
+		testCase{
+			description: "Test Case: four digits number",
+			given:       "2701",
+			expected:    "iki min yeddi yüz bir",
+		},
+		testCase{
+			description: "Test Case: four digits number",
+			given:       "3210",
+			expected:    "üç min iki yüz on",
+		},
+		testCase{
+			description: "Test Case: three digits number",
+			given:       "4850",
+			expected:    "dörd min səkkiz yüz əlli",
+		},
+		testCase{
+			description: "Test Case: three digits number",
+			given:       "5412",
+			expected:    "beş min dörd yüz on iki",
+		},
+		testCase{
+			description: "Test Case: three digits number",
+			given:       "9594",
+			expected:    "doqquz min beş yüz doxsan dörd",
+		},
+		testCase{
+			description: "Test Case: three digits number",
+			given:       "8670",
+			expected:    "səkkiz min altı yüz yetmiş",
+		},
+		testCase{
+			description: "Test Case: three digits number",
+			given:       "7001",
+			expected:    "yeddi min bir",
+		},
+		testCase{
+			description: "Test Case: three digits number",
+			given:       "8200",
+			expected:    "səkkiz min iki yüz",
+		},
+		testCase{
+			description: "Test Case: three digits number",
+			given:       "9526",
+			expected:    "doqquz min beş yüz iyirmi altı",
+		}}
+}
+
+func testCasesForFiveDigitsNumbers() []testCase {
+	return []testCase{
+		testCase{
+			description: "Test Case: four digits number",
+			given:       "10000",
+			expected:    "on min",
+		},
+		testCase{
+			description: "Test Case: four digits number",
+			given:       "11111",
+			expected:    "on bir min bir yüz on bir",
+		},
+		testCase{
+			description: "Test Case: four digits number",
+			given:       "10192",
+			expected:    "on min bir yüz doxsan iki",
+		},
+		testCase{
+			description: "Test Case: four digits number",
+			given:       "27021",
+			expected:    "iyirmi yeddi min iyirmi bir",
+		},
+		testCase{
+			description: "Test Case: four digits number",
+			given:       "13210",
+			expected:    "on üç min iki yüz on",
+		},
+		testCase{
+			description: "Test Case: three digits number",
+			given:       "48550",
+			expected:    "qırx səkkiz min beş yüz əlli",
+		},
+		testCase{
+			description: "Test Case: three digits number",
+			given:       "95412",
+			expected:    "doxsan beş min dörd yüz on iki",
+		},
+		testCase{
+			description: "Test Case: three digits number",
+			given:       "79594",
+			expected:    "yetmiş doqquz min beş yüz doxsan dörd",
+		},
+		testCase{
+			description: "Test Case: three digits number",
+			given:       "86070",
+			expected:    "səksən altı min yetmiş",
+		},
+		testCase{
+			description: "Test Case: three digits number",
+			given:       "70010",
+			expected:    "yetmiş min on",
+		},
+		testCase{
+			description: "Test Case: three digits number",
+			given:       "81200",
+			expected:    "səksən bir min iki yüz",
+		},
+		testCase{
+			description: "Test Case: three digits number",
+			given:       "10001",
+			expected:    "on min bir",
 		}}
 }

f82c21e05409b2cbdb407bcd3d8e2bf10660a004
mehdiyevcs <jalal.mehdiyev.s@gmail.com>
2023-03-09 01:47:58 +0400

    #8: xCorrelationId header intercepted and added to response headers

--- cmd/aznum2words-webapp/config/config.go
+++ cmd/aznum2words-webapp/config/config.go
@@ -2,14 +2,10 @@ package config
 
 import "github.com/alexflint/go-arg"
 
-// Opts ...
-var Opts struct {
-	Profile string `short:"p" long:"profile" default:"default" description:"Application run profile"`
-}
-
 // Config holds a configuration variables.
 type Config struct {
 	Port string `arg:"env:PORT,required"`
+	Host string `arg:"env:HOST, required"`
 }
 
 // Conf ...

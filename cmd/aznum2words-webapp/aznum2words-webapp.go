caeb8f5ba3fff95f0ffedad9d8a65e310520d661
mehdiyevcs <jalal.mehdiyev.s@gmail.com>
2023-03-11 18:27:28 +0400

    #8: Folder structure refactored

--- cmd/aznum2words-webapp/aznum2words-webapp.go
+++ cmd/aznum2words-webapp/aznum2words-webapp.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/alexliesenfeld/health"
 	"github.com/egasimov/aznum2words/cmd/aznum2words-webapp/api"
 	"github.com/egasimov/aznum2words/cmd/aznum2words-webapp/config"
+	"github.com/egasimov/aznum2words/cmd/aznum2words-webapp/handler"
+	"github.com/egasimov/aznum2words/cmd/aznum2words-webapp/router"
 	"github.com/jessevdk/go-flags"
 	"github.com/joho/godotenv"
-	"github.com/labstack/echo/v4"
-	echomiddleware "github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	"os"
 )
-
-var LoggerConfig = echomiddleware.LoggerConfig{
-	Format: `{"time":"${time_rfc3339_nano}","x-correlation-id":"${header:x-correlation-id}","remote_ip":"${remote_ip}",` +
-		`"host":"${host}","method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
-		`"status":${status},"error":"${error}","latency":${latency},"latency_human":"${latency_human}"}` + "\n",
-	CustomTimeFormat: "2006-01-02 15:04:05.00000",
-}
 
 func main() {
 	var err error
@@ -34,22 +24,12 @@ func main() {
 	// Create an instance of our handler which satisfies the generated interface
 	var azNum2WordsApi = new(api.AzNum2WordsApi)
 
-	// This is how you set up a basic Echo router
-	e := echo.New()
-	//Add x-correlation-id header to response
-	e.Use(echomiddleware.RequestIDWithConfig(
-		echomiddleware.RequestIDConfig{
-			TargetHeader: echo.HeaderXCorrelationID,
-		}))
-	// Log all requests
-	e.Use(echomiddleware.LoggerWithConfig(LoggerConfig))
-
-	// We now register our api above as the handler for the interface
-	api.RegisterHandlers(e, azNum2WordsApi)
-	registerHealthCheckProbes(e)
+	r := router.New()
+	h := handler.NewHandler(azNum2WordsApi)
+	h.Register(r)
 
 	// And we serve HTTP until the world ends.
-	e.Logger.Fatal(e.Start(config.Conf.Host + ":" + config.Conf.Port))
+	r.Logger.Fatal(r.Start(config.Conf.Host + ":" + config.Conf.Port))
 }
 
 func initEnvVars() {
@@ -63,14 +43,4 @@ func initEnvVars() {
 	} else {
 		log.Info("Environment variables overloaded from: ", profileFileName)
 	}
-}
-
-func registerHealthCheckProbes(e *echo.Echo) {
-	checker := health.NewChecker(
-		// Set a status listener that will be invoked when the health status changes.
-		health.WithStatusListener(func(ctx context.Context, state health.CheckerState) {
-			log.Info(fmt.Sprintf("health status changed to %s", state.Status))
-		}),
-	)
-	e.GET("/health", echo.WrapHandler(health.NewHandler(checker)))
 }
---
+++ cmd/aznum2words-webapp/router/router.go
@@ -0,0 +1,25 @@
+package router
+
+import (
+	"github.com/labstack/echo/v4"
+	echomiddleware "github.com/labstack/echo/v4/middleware"
+)
+
+var LoggerConfig = echomiddleware.LoggerConfig{
+	Format: `{"time":"${time_rfc3339_nano}","x-correlation-id":"${header:x-correlation-id}","remote_ip":"${remote_ip}",` +
+		`"host":"${host}","method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
+		`"status":${status},"error":"${error}","latency":${latency},"latency_human":"${latency_human}"}` + "\n",
+	CustomTimeFormat: "2006-01-02 15:04:05.00000",
+}
+
+func New() *echo.Echo {
+	e := echo.New()
+	e.Use(echomiddleware.RequestIDWithConfig(
+		echomiddleware.RequestIDConfig{
+			TargetHeader: echo.HeaderXCorrelationID,
+		}))
+	// Log all requests
+	e.Use(echomiddleware.LoggerWithConfig(LoggerConfig))
+
+	return e
+}
